f972d7cb93167fea3db78c1069935eb93f3bd0fe
Matija <milinkovicmatija22@gmail.com>
2026-03-17 21:42:03 +0100

    allowed all protocols for egress and set tcp for ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,16 +51,16 @@ resource "aws_security_group" "my_security_group" {
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
+    from_port   = 0 
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
