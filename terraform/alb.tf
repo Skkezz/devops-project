9d5c714d53997e364e0d1bff3af773b0f0ffe93c
matija <milinkovicmatija22@gmail.com>
2026-04-09 22:16:13 +0200

    added ssl https

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.my_vpc.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -39,13 +39,18 @@ resource "aws_lb" "front" {
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.my_public_subnet.id, aws_subnet.my_public2_subnet.id]
 
+  lifecycle {
+    prevent_destroy = false
+  }
   
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.front.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:eu-central-1:342671697118:certificate/01aced37-6bad-4df4-8826-8ee2f7154ecb"
   
   default_action {
     type             = "forward"
@@ -56,5 +61,5 @@ resource "aws_lb_listener" "http" {
 resource "aws_lb_target_group_attachment" "ec2_attach" {
   target_group_arn = aws_lb_target_group.main.arn
   target_id        = aws_instance.my_private_ec2.id
-  port             = 80
+  port             = 80 // za nginx
 }
